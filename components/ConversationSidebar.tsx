1b39ed5b0bd6feb15618720ff8f32cc71227fdd1
Daniel Duma <danielduma@gmail.com>
2025-12-09 18:50:19 +0300

    Minor adjustments in ConversationSidebar and ImageAttachment components for consistency.

--- components/ConversationSidebar.tsx
+++ components/ConversationSidebar.tsx
@@ -124,3 +124,4 @@ export const ConversationSidebar: React.FC<ConversationSidebarProps> = ({
 };
 
 
+
--- components/ImageAttachment.tsx
+++ components/ImageAttachment.tsx
@@ -38,3 +38,4 @@ export const ImageAttachment: React.FC<ImageAttachmentProps> = ({ files, onRemov
 };
 
 
+
